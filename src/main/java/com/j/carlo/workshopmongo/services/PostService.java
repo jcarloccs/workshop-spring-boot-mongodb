1e832e060d509010b2f28214d1536a311676749a
jcarloccs <jcarlo@outlook.com>
2023-01-23 11:47:03 -0300

    Obtendo um post por id

---
+++ src/main/java/com/j/carlo/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.j.carlo.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.j.carlo.workshopmongo.domain.Post;
+import com.j.carlo.workshopmongo.repository.PostRepository;
+import com.j.carlo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+}
